552b6c714d06569fdb6ad29694ff2f10decedd8c
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-03-18 00:51:33 -0500

    Try to add close event on js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,13 @@
+const body = document.body;
+
+function toggleModal() {
+  const buttonClose = body.querySelector('.xbutton');
+  buttonClose.addEventListener('click', function () {
+    body.classList.remove('show-overlay');
+  })
+
+  setTimeout(function () {
+    body.classList.add('show-overlay');
+  }, 1000)
+}
 
